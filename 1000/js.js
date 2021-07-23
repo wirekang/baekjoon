9d8ecb05c4893ef4ab2991abe24a8fe4c7835f0d
wirekang <wirekang@gmail.com>
2021-07-23 19:19:06 +0900

    Init

---
+++ 1000/js.js
@@ -0,0 +1,5 @@
+const fs = require("fs")
+const input = fs.readFileSync("/dev/stdin").toString().split(" ")
+const a = parseInt(input[0], 10)
+const b = parseInt(input[1], 10)
+console.log(a + b)
---
+++ js.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+dir=$(dirname $(realpath $0))
+pDir=$dir/$PROBLEM
+
+out=$(cat $pDir/in | node $pDir/js.js)
+rOut=$(cat $pDir/out)
+
+echo "Problem : $PROBLEM"
+echo "Language: node.js"
+
+if [[ "$out" == "$rOut" ]]; then
+  echo "PASS"
+else
+  echo "FAIL"
+  echo "----"
+  echo $out
+  echo "----"
+  echo $rOut
+fi
